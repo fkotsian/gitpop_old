0d0973efe8b43d2cb5ac76c21867642b63e7df01
Frank Kotsianas <frankkotsianas@gmail.com>
2016-06-12 22:19:05 -0700

    Setup promise mocking helpers

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -1,13 +1,36 @@
 require('babel-core/register');
 require('babel-polyfill');
 require('jasmine-ajax');
+require('pivotal-js-jasmine-matchers');
+
+const MockPromises = require('mock-promises');
+const ES6Promise = require('es6-promise');
+const Deferred = require('./app/support/deferred');
+
+let globals;
+
+beforeAll(function() {
+  ES6Promise.polyfill();
+
+  globals = {
+    Deferred,
+    MockPromises
+  };
+  Object.assign(global, globals);
+});
 
 beforeEach(function() {
   jasmine.Ajax.install();
   jasmine.clock().install();
+  MockPromises.install(Promise);
 });
 
 afterEach(function() {
   jasmine.Ajax.uninstall();
   jasmine.clock().uninstall();
+  MockPromises.reset();
+});
+
+afterAll(function() {
+  Object.keys(globals).forEach(key => delete global[key]);
 });
